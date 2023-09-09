7f9a0882ebd0e46b26ffe5a207125643e8d7ec27
Lakello <59009343+Lakello@users.noreply.github.com>
2023-09-09 15:14:14 +0700

    add yg plugin

--- Assets/Source/Scripts/Game/GameInit.cs
+++ Assets/Source/Scripts/Game/GameInit.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
-using YG;
 
 public class GameInit : MonoBehaviour
 {
-    private void OnEnable() => YandexGame.GetDataEvent += EndInit;
+    //private void OnEnable() => YandexGame.GetDataEvent += EndInit;
 
-    private void OnDisable() => YandexGame.GetDataEvent -= EndInit;
+    //private void OnDisable() => YandexGame.GetDataEvent -= EndInit;
 
     private void EndInit() => IJunior.TypedScenes.Game.Load<MenuState>();
 }
--- Assets/Source/Scripts/Yandex/Saves/Saves.cs
+++ Assets/Source/Scripts/Yandex/Saves/Saves.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using UnityEngine;
-using YG;
+﻿using UnityEngine;
 
 public sealed class Saves : MonoBehaviour, IReadFromArray<LevelInfo>, IRead<CurrentLevel>, IWrite<LevelInfo>, IWrite<CurrentLevel>
 {
-    public LevelInfo Read(int index) => YandexGame.savesData.LevelInfo[index];
+    public LevelInfo Read(int index) => default;//YandexGame.savesData.LevelInfo[index];
 
-    public CurrentLevel Read() => YandexGame.savesData.CurrentLevelIndex;
+    public CurrentLevel Read() => default;// YandexGame.savesData.CurrentLevelIndex;
 
     public void Write(LevelInfo value)
     {
@@ -15,44 +13,44 @@ public sealed class Saves : MonoBehaviour, IReadFromArray<LevelInfo>, IRead<Curr
         else
             AddLevelInfo(value);
     }
-
-    public void Write(CurrentLevel value) => YandexGame.savesData.CurrentLevelIndex = value;
+    CurrentLevel i;
+    public void Write(CurrentLevel value) => i = value;// YandexGame.savesData.CurrentLevelIndex = value;
 
     private bool ContainsLevelIndex(int index, out int levelInfoIndex)
     {
         levelInfoIndex = -1;
 
-        var levelInfos = YandexGame.savesData.LevelInfo;
+        //var levelInfos = YandexGame.savesData.LevelInfo;
 
-        for (int i = 0; i < levelInfos.Length; i++)
-        {
-            if (levelInfos[i].LevelIndex == index)
-            {
-                levelInfoIndex = i;
-                return true;
-            }
-        }
+        //for (int i = 0; i < levelInfos.Length; i++)
+        //{
+        //    if (levelInfos[i].LevelIndex == index)
+        //    {
+        //        levelInfoIndex = i;
+        //        return true;
+        //    }
+        //}
 
         return false;
     }
 
     private void AddLevelInfo(LevelInfo value)
     {
-        var levelInfo = YandexGame.savesData.LevelInfo;
-        var newLevelInfo = new LevelInfo[levelInfo.Length + 1];
+        //var levelInfo = YandexGame.savesData.LevelInfo;
+        //var newLevelInfo = new LevelInfo[levelInfo.Length + 1];
 
-        for (int i = 0; i < levelInfo.Length; i++)
-        {
-            newLevelInfo[i] = levelInfo[i];
-        }
+        //for (int i = 0; i < levelInfo.Length; i++)
+        //{
+        //    newLevelInfo[i] = levelInfo[i];
+        //}
 
-        newLevelInfo[newLevelInfo.Length - 1] = value;
+        //newLevelInfo[newLevelInfo.Length - 1] = value;
 
-        YandexGame.savesData.LevelInfo = newLevelInfo;
+        //YandexGame.savesData.LevelInfo = newLevelInfo;
     }
 
     private void UpdateLevelInfo(LevelInfo value, int index)
     {
-        YandexGame.savesData.LevelInfo[index] = value;
+        //YandexGame.savesData.LevelInfo[index] = value;
     }
 }
