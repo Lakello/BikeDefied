57c562f50fd25af582d79a7724a427ffb27a3188
Lakello <59009343+Lakello@users.noreply.github.com>
2023-09-30 21:35:16 +0700

    Педали крутит

---
+++ Assets/Source/Scripts/Bike/BikePedalRotator.cs
@@ -0,0 +1,32 @@
+﻿using Reflex.Attributes;
+using UnityEngine;
+
+public class BikePedalRotator : MonoBehaviour
+{
+    [SerializeField] private Transform _leftPedal;
+    [SerializeField] private Transform _rightPedal;
+    [SerializeField] private AnimationCurve _rotationCurve;
+    [SerializeField] private float _maxVelocity;
+
+    private Rigidbody _bikeRigidbody;
+    private float _angle;
+
+    private float NormalVelocity => Mathf.Abs(_bikeRigidbody.velocity.z / _maxVelocity);
+    private float Direction => Mathf.Clamp(_bikeRigidbody.velocity.z, -1, 1);
+
+    [Inject]
+    private void Inject(Bike bike)
+    {
+        _bikeRigidbody = bike.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _angle = _rotationCurve.Evaluate(NormalVelocity);
+
+        transform.rotation *= Quaternion.AngleAxis(_angle * -Direction, Vector3.right);
+
+        _leftPedal.rotation = Quaternion.LookRotation(_bikeRigidbody.transform.forward, Vector3.up);
+        _rightPedal.rotation = Quaternion.LookRotation(_bikeRigidbody.transform.forward, Vector3.up);
+    }
+}
